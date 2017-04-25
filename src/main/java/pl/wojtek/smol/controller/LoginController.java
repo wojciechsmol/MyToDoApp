94c7411ed8c353be809803eebb9443decfc1ff4d
wojciechsmol <wojciechsmolcontact@gmail.com>
2017-04-26 00:32:14 +0200

    first commit

---
+++ src/main/java/pl/wojtek/smol/controller/LoginController.java
@@ -0,0 +1,15 @@
+package pl.wojtek.smol.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String loginMessage() {
+        return "login";
+    }
+}

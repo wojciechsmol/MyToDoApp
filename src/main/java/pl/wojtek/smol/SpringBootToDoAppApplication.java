7ae8d6e0bf65b42bc4abbb4b3f9cf030a77f1fb8
wojciechsmol <wojciechsmolcontact@gmail.com>
2017-04-25 22:29:13 +0200

    Initial commit

---
+++ src/main/java/pl/wojtek/smol/SpringBootToDoAppApplication.java
@@ -0,0 +1,12 @@
+package pl.wojtek.smol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootToDoAppApplication.class, args);
+	}
+}
